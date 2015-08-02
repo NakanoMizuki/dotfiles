8bb73eb54e617ce6f902d9c2ff53672379ea36d7
MizukiNakano <mizuki590@gmail.com>
2015-08-02 14:34:01 +0900

    Modified mklink.bat

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink /D %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
-mklink /D %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
-exit 0
+mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+pause
