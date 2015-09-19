5a9561656ff48f40b82bad1e1945fa1847e0881a
MizukiNakano <mizuki590@gmail.com>
2015-09-19 09:58:34 +0900

    mod script executing directory

--- mac-provisioning/provisioning.sh
+++ mac-provisioning/provisioning.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# move current directory
+pushd `dirname $0`
+
 # set install directory
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 ansible-playbook -i hosts -vv initialized.yml
+
+
+popd
