092f3c9d94f9f796abf98a82d1df978990632ba2
MizukiNakano <mizuki590@gmail.com>
2015-12-14 18:10:24 +0900

    add .latexmkrc

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+mklink %HOMEPATH%"\.latexmkrc" %HOMEPATH%"\dotfiles\.latexmkrc"
 pause
